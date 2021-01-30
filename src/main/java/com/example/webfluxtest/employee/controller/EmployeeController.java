c5066d2a150d92062306bb52dc170a0cd6ac5e08
Incheol Jung <incheol.jung@navercorp.com>
2021-01-31 02:12:59 +0900

    update

--- src/main/java/com/example/webfluxtest/employee/controller/EmployeeController.java
+++ src/main/java/com/example/webfluxtest/employee/controller/EmployeeController.java
@@ -1,57 +1,37 @@
 package com.example.webfluxtest.employee.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.webfluxtest.employee.entity.Employee;
 import com.example.webfluxtest.employee.model.InsertEmployeeRequest;
 import com.example.webfluxtest.employee.service.ReactiveEmployeeService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
-
-// import com.example.webfluxtest.employee.service.ReactiveEmployeeService;
 
 /**  * EmployeeController
  *
  * @author incheol.jung
  * @since 2021. 01. 29.
  */
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("employees")
 public class EmployeeController {
-	// private final EmployeeService employeeService;
 	private final ReactiveEmployeeService reactiveEmployeeService;
 
-	// @GetMapping
-	// public ResponseEntity<List<Employee>> get() {
-	// 	return new ResponseEntity(employeeService.get(), HttpStatus.OK);
-	// }
-	//
-	// @PostMapping
-	// public ResponseEntity<Employee> insert(@RequestBody InsertEmployeeRequest request) {
-	// 	return new ResponseEntity(employeeService.insert(request), HttpStatus.ACCEPTED);
-	// }
-
-	// @PostMapping("reactiveService")
-	// @ResponseBody
-	// public Mono<ResponseEntity> insertWithService(@RequestBody InsertEmployeeRequest request) {
-	// 	return Mono.just(new ResponseEntity(reactiveEmployeeService.insertWithService(request), HttpStatus.ACCEPTED));
-	// }
-	//
 	@PostMapping("r2dbcRepository")
 	@ResponseBody
-	public Mono<ResponseEntity> insertWithR2dbcRepository(@RequestBody InsertEmployeeRequest request) {
-		return Mono.just(
-			new ResponseEntity(
-				reactiveEmployeeService.insertWithR2dbcRepository(request)
-					.publishOn(Schedulers.parallel())
-					.subscribeOn(Schedulers.boundedElastic()),
-				HttpStatus.ACCEPTED));
+	public Mono<Employee> insertWithR2dbcRepository(@RequestBody InsertEmployeeRequest request) {
+		log.info("EmployeeController > insertWithR2dbcRepository()");
+		return Mono.defer(() -> reactiveEmployeeService.insertWithR2dbcRepository(request))
+			.publishOn(Schedulers.parallel())
+			.subscribeOn(Schedulers.boundedElastic());
 	}
 }
--- src/main/java/com/example/webfluxtest/employee/service/ReactiveEmployeeService.java
+++ src/main/java/com/example/webfluxtest/employee/service/ReactiveEmployeeService.java
@@ -1,13 +1,12 @@
 package com.example.webfluxtest.employee.service;
 
-import java.util.function.Consumer;
-
 import org.springframework.stereotype.Service;
 
 import com.example.webfluxtest.employee.entity.Employee;
 import com.example.webfluxtest.employee.model.InsertEmployeeRequest;
 import com.example.webfluxtest.employee.reactive.EmployeeR2dbcRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
@@ -16,18 +15,15 @@ import reactor.core.scheduler.Schedulers;
  * @author incheol.jung
  * @since 2021. 01. 28.
  */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ReactiveEmployeeService {
-	// private final EmployeeService employeeService;
 
 	private final EmployeeR2dbcRepository employeeR2dbcRepository;
 
-	// public Mono<Employee> insertWithService(InsertEmployeeRequest request) {
-	// 	return monoConsumer(o -> employeeService.insert(request));
-	// }
-
 	public Mono<Employee> insertWithR2dbcRepository(InsertEmployeeRequest request) {
+		log.info("EmployeeService > insertWithR2dbcRepository()");
 		Employee employee = Employee.builder()
 			.name(request.getName())
 			.email(request.getEmail())
@@ -42,15 +38,8 @@ public class ReactiveEmployeeService {
 
 		return Mono.just(employeeR2dbcRepository.save(employee).block())
 			.publishOn(Schedulers.parallel())
-			.subscribeOn(Schedulers.boundedElastic());
-	}
-
-	private Mono monoConsumer(Consumer consumer) {
-		return Mono.just(Mono
-			.empty()
-			.then()
-			.doOnSuccess(consumer)
-			.subscribe());
+			.subscribeOn(Schedulers.boundedElastic())
+			.log();
 	}
 
 }
